c8e85196e5ee5b88a5c6144d31fca57e3c3baf74
Yassine Khlify <contact.louajet@gmail.com>
2025-08-16 13:47:04 +0100

    href updated

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "966002035"
+    serviceWorkerVersion: "619153837"
   }
 });
