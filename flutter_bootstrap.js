cd83fff24f1de5eeb4c48bd734f143db8c2318b9
Yassine Khlify <yessine.khelifi@istic.ucar.tn>
2025-08-16 13:10:33 +0100

    first commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2438018991"
+    serviceWorkerVersion: "966002035"
   }
 });
